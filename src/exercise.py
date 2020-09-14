a003baffcd61d9659eed715fa46e145dcd9b0aa0
tynankahar <19789@bridgend.ac.uk>
2020-09-14 10:39:29 +0000

    completed exersise 1.6

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #write your code below this line
     message = input("Write a message...")
-
+    print(message)
+    print(message)
+    print(message)
+    
 if __name__ == '__main__':
     main()
